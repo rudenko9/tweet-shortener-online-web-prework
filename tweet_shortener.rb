50b63d46a4b593abbdbe4b88ad25e495eb41490d
Nataliya R <n.rudenko@gmail.com>
2019-02-05 20:34:10 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,2 +1,6 @@
 # Write your code here.
 phrase = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+def dictionary
+  converst = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"
+  }
+end 
