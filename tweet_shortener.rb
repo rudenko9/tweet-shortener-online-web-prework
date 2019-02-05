c1b259994e7006c578c2766fb6ac5c335db1e05f
Nataliya R <n.rudenko@gmail.com>
2019-02-05 21:19:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,6 +4,7 @@ def dictionary
   converst = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"
   }
 end 
+
 require "pry"
 
 def word_substituter(tweet)
