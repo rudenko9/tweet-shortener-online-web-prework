cd339cc914937a074a4b297d97056af445e5e17d
Nataliya R <n.rudenko@gmail.com>
2019-02-05 21:19:27 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,9 +3,7 @@ phrase = "Hey guys, can anyone teach me how to be cool? I really want to be the 
 def dictionary
   converst = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"
   }
-end 
-
-require "pry"
+end
 
 def word_substituter(tweet)
   tweet.split(" ").map do |word|
